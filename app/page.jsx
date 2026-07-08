c0569d4808879ddd6e39f2c17628938b1756d8eb
leticiacaceres <leticiacaceres38@gmail.com>
2026-07-08 12:27:44 -0300

    Refactor Home component layout and styles
    Updated the Home component to modify styles and content.

---
+++ app/page.jsx
@@ -0,0 +1,7 @@
+   export default function Home() {
+     return (
+       <main style={{color: 'white', textAlign: 'center', padding: '50px'}}>
+         <h1>CINECLÃ 🍿</h1>
+       </main>
+     )
+   }
